5abdfe29045c788dc3b0e49a2dafcd75c70434a0
Swathi Katakam <swathik2322@gmail.com>
2024-02-08 15:15:51 -0600

    initial commit

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -7,14 +7,23 @@ export const Celebrities = () => {
 
     for (const star of celebrities) {
         html += `<li 
-                    data-id="${celebrity.id}" 
+                    data-id="${star.id}" 
                     data-type="celebrity"
-                    data-sport="${celebrty.sport}"
-                    id="star--${celebrity.id}">
-                    ${celebrity.name}
+                    data-name="${star.name}"
+                    data-sport="${star.sport}"
+                    id="star--${star.id}">
+                    ${star.name}
                 </li>`
     }
 
     html += "</ol>"
     return html
 }
+
+document.addEventListener("click",(celebrityClickEvent)=>{
+    const celebrityElement=celebrityClickEvent.target;
+    if(celebrityElement.dataset.type==="celebrity"){
+        window.alert(`${celebrityElement.dataset.name} is a ${celebrityElement.dataset.sport}`)
+    }
+
+})
--- scripts/Kids.js
+++ scripts/Kids.js
@@ -6,10 +6,20 @@ export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
+        html += `<li data-id="${child.id}" 
+                     data-type="child" 
+                     data-name="${child.name}" 
+                     data-wish="${child.wish}">${child.name}</li>`
     }
 
     html += "</ol>"
     return html
 }
+
+document.addEventListener("click",(kidClickEvent)=>{
+    const kidElement=kidClickEvent.target;
+    if(kidElement.dataset.type==="child"){
+        window.alert(`${kidElement.dataset.name} wish is ${kidElement.dataset.wish}`)
+    }
+})
 
